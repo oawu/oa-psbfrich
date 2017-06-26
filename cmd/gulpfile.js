2e46229c82f9b7a4314b2e7deb5046329352ed53
OA Wu <comdan66@gmail.com>
2017-06-26 15:03:40 +0800

    Finish.

--- cmd/gulpfile.js
+++ cmd/gulpfile.js
@@ -133,8 +133,9 @@ gulp.task ('watch', function () {
 
 gulp.task ('update_icomoon_font_icon', function () {
   read ('./root/font/icomoon/style.css', 'utf8', function (err, buffer) {
-    var t = buffer.match (/\.icon-[a-zA-Z_\-0-9]*:before\s?\{\s*content:\s*"[\\A-Za-z0-9]*";\s*}/g);
+    var t = buffer.match (/\.icon-[a-zA-Z_\-0-9]*:before\s?\{\s*content:\s*"[\\A-Za-z0-9]*";(\s*color:\s*#[A-Za-z0-9]*;)?\s*}/g);
       if (!(t && t.length)) return;
+console.error (t);
 
       writeFile ('./root/scss/icon.scss', '@import "_oa";\n\n@include font-face("icomoon", font-files("icomoon/fonts/icomoon.eot", "icomoon/fonts/icomoon.woff", "icomoon/fonts/icomoon.ttf", "icomoon/fonts/icomoon.svg"));\n[class^="icon-"], [class*=" icon-"] {\n  font-family: "icomoon"; speak: none; font-style: normal; font-weight: normal; font-variant: normal;\n  @include font-smoothing(antialiased);\n}\n\n' + t.join ('\n'), function(err) {
         if (err) console.log ('\n ' + colors.red ('•') + colors.red (' [錯誤] ') + '寫入檔案失敗！');
